28e4db983897085dded58d6395849b85803c8ab1
Arthur Law <arthur.dartora.tds2023@gmail.com>
2025-07-12 20:12:52 -0300

    Mensagens de error

---
+++ mensagens.php
@@ -0,0 +1,7 @@
+<?php 
+
+$mensagens = [
+    'Erro 0: Não foi possível criar diretórios',
+    'Erro 1: Falha ao conectar ao banco de dados',
+    'Projeto criado: faça download clicando <a href="sistema.zip">aqui</a>'
+];
